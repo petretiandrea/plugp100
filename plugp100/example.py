a0e2215ffe46f4cf5b3b0a04e54f4a7543cc5b5d
Andrea <petretiandrea@gmail.com>
2022-01-10 21:25:57 +0100

    trying multiarch ci

--- plugp100/example.py
+++ plugp100/example.py
@@ -1,17 +1,20 @@
 import asyncio
 
-from plugp100 import TapoApiClient
+from plugp100 import TapoApiClient, TapoApiDiscover
 
 
 async def main():
     # create generic tapo api
-    sw = TapoApiClient("<ip>", "<email>", "<passwd>")
+    # is_valid_device = await TapoApiDiscover.is_tapo_device("192.168.1.10")
+    # print(is_valid_device)
+
+    sw = TapoApiClient("192.168.1.104", "andreapedro96@gmail.com", "Andbea9671!")
     await sw.login()
-    await sw.on()
-    await sw.set_brightness(100)
+    print(await sw.get_state_as_dict())
     print(await sw.get_state())
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-loop.run_until_complete(asyncio.sleep(0.1))
-loop.close()
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.run_until_complete(asyncio.sleep(0.1))
+    loop.close()
