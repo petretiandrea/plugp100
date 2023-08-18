e254310f660cefc680126656ebe5eab1e6db70d0
Teemu Rytilahti <tpr@iki.fi>
2023-08-18 18:46:13 +0300

    Make more deviceinfo response fields accessible

--- plugp100/responses/device_state.py
+++ plugp100/responses/device_state.py
@@ -89,20 +89,48 @@ class LedStripDeviceState(DeviceState):
 @dataclass
 class DeviceInfo:
     device_id: str
+    hardware_id: str
+    oem_id: str
+    timezone: str
     firmware_version: str
     hardware_version: str
+    ip: str
     mac: str
     nickname: str
     model: str
     type: str
     overheated: bool
+
+    # wifi
+    ssid: str
     signal_level: int
     rssi: int
     friendly_name: str
+    
+    # location data
+    latitude: int
+    longitude: int
+    timezone: str
+    time_difference: int
+    language: str
+
+    # device state
+    power_protection_status: str
+    device_on: bool
+    on_time: int
+
+    # autooff
+    auto_off: bool
+    auto_off_time_remaining: int
+
     is_hardware_v2: bool = property(lambda self: self.hardware_version == "2.0")
+
+
 
     def __init__(self, **kwargs):
         self.device_id = kwargs["device_id"]
+        self.hardware_id = kwargs['hw_id']
+        self.oem_id = kwargs['oem_id']
         self.firmware_version = kwargs["fw_ver"]
         self.hardware_version = kwargs["hw_ver"]
         self.mac = kwargs["mac"]
@@ -110,9 +138,27 @@ class DeviceInfo:
         self.model = kwargs["model"]
         self.type = kwargs["type"]
         self.overheated = kwargs.get("overheated", False)
+        self.power_protection_status = kwargs["power_protection_status"]
+        self.ip = kwargs['ip']
+        self.ssid = base64.b64decode(kwargs["ssid"]).decode()
         self.signal_level = kwargs.get("signal_level", 0)
         self.rssi = kwargs.get("rssi", 0)
         self.friendly_name = self.model if self.nickname == "" else self.nickname
+
+        self.latitude = kwargs["latitude"]
+        self.longitude = kwargs["longitude"]
+        self.timezone = kwargs["region"]
+        self.time_difference = kwargs["time_diff"]
+        self.language = kwargs["lang"]
+
+        # device state
+        self.device_on = kwargs["device_on"]
+        self.on_time = kwargs["on_time"]
+
+        # auto off
+        self.auto_off = (kwargs["auto_off_status"] == "on")
+        self.auto_off_time_remaining = kwargs["auto_off_remain_time"]
+
 
     def get_semantic_firmware_version(self) -> semantic_version.Version:
         pieces = self.firmware_version.split("Build")
