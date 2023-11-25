483ac266a6e0697fd48f424430feeb7978376d03
petretiandrea <petretiandrea@gmail.com>
2023-11-25 13:24:54 +0100

    improved https suppport

--- tests/unit/test_hub_h100.py
+++ tests/unit/test_hub_h100.py
@@ -10,10 +10,11 @@ from tests.unit.test_utils import wrap_as_tapo_response, generate_random_childre
 
 
 class HubH100Test(unittest.IsolatedAsyncioTestCase):
-
     async def asyncSetUp(self) -> None:
         self._mock_protocol = AsyncMock(TapoProtocol)
-        api = TapoClient(auth_credential=AuthCredential("", ""), ip_address="", protocol=self._mock_protocol)
+        api = TapoClient(
+            auth_credential=AuthCredential("", ""), url="", protocol=self._mock_protocol
+        )
         self.hub = HubDevice(api=api, subscription_polling_interval_millis=1000)
 
     async def asyncTearDown(self):
@@ -21,10 +22,10 @@ class HubH100Test(unittest.IsolatedAsyncioTestCase):
 
     async def test_should_get_all_children(self):
         children = generate_random_children(10, 55)
-        self._mock_protocol.send_request.side_effect = [wrap_as_tapo_response(it) for it in children]
+        self._mock_protocol.send_request.side_effect = [
+            wrap_as_tapo_response(it) for it in children
+        ]
 
         expected = reduce(lambda x, y: x.merge(y), children)
         state = (await self.hub.get_children()).get_or_raise()
         self.assertListEqual(state.child_device_list, expected.child_device_list)
-
-
