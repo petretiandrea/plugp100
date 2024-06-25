e49216c6afabd65e29a699d204f35b3095ce5b76
Brian Ma <chma0@nuvoton.com>
2024-06-25 11:31:34 +0800

    fix(core): handle authentication failed case
    Fix InvalidAuthentication init fail and unclosed client sessions.
    
    Signed-off-by: Brian Ma <chma0@nuvoton.com>

--- plugp100/new/errors/invalid_authentication.py
+++ plugp100/new/errors/invalid_authentication.py
@@ -1,3 +1,3 @@
 class InvalidAuthentication(Exception):
     def __init__(self, host: str, device_type: str):
-        super(f"Invalid authentication error for ${host}, ${device_type}")
+        super().__init__(f"Invalid authentication error for ${host}, ${device_type}")
