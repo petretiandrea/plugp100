c6022b8027118b109adfe9b5e61e201444387fd9
hazzabeee <82228468+hazzabeee@users.noreply.github.com>
2024-07-11 21:05:22 +0100

    Allow async_make_post to accept custom headers
    Also better error handling

--- plugp100/common/utils/http_client.py
+++ plugp100/common/utils/http_client.py
@@ -1,7 +1,9 @@
 from typing import Any
 
 import aiohttp
-
+import logging
+import ssl
+logger = logging.getLogger(__name__)
 
 class AsyncHttp:
     def __init__(self, session: aiohttp.ClientSession):
@@ -13,12 +15,17 @@ class AsyncHttp:
             "Accept": "application/json",
         }
 
-    async def async_make_post(self, url, json: Any) -> aiohttp.ClientResponse:
+    async def async_make_post(self, url, json: Any, headers=None) -> aiohttp.ClientResponse:
         self.session.cookie_jar.clear()
-        async with self.session.post(
-            url, json=json, headers=self.common_headers
-        ) as response:
-            return await self._force_read_release(response)
+        if headers is None:
+            headers = self.common_headers
+        try:
+            async with self.session.post(
+                url, json=json, headers=headers
+            ) as response:
+                return await self._force_read_release(response)
+        except Exception as e:
+            logger.warning(f"POST failed: {type(e).__name__} {e}")
 
     async def async_make_post_cookie(self, url, json, cookie) -> aiohttp.ClientResponse:
         self.session.cookie_jar.clear()
